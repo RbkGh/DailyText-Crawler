377e8f33912096bffdbd67202320e077b9f866d3
Ace P Rbk <rbk.unlimited@gmail.com>
2017-01-20 22:22:32 +0000

    Added method to crawl dailyText and return in Friendly Format,untested

--- src/main/java/com/swiftpot/dailytext/crawler/DailyTextCrawler.java
+++ src/main/java/com/swiftpot/dailytext/crawler/DailyTextCrawler.java
@@ -1,5 +1,15 @@
 package com.swiftpot.dailytext.crawler;
 
+import com.swiftpot.dailytext.crawler.models.DailyTextEntity;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * @author Ace Programmer Rbk
  *         <Rodney Kwabena Boachie at [rodney@swiftpot.com,rbk.unlimited@gmail.com]> on
@@ -7,5 +17,29 @@ package com.swiftpot.dailytext.crawler;
  */
 public class DailyTextCrawler {
 
+    private static String JW_DAILY_TEXT_BASE_URL="http://wol.jw.org/en/wol/dt/r1/lp-e";
+    private static String DATE_PATTERN = "yyyy-MM-dd";
 
+    /**
+     *
+     * @param date pass in date in yyyy/mm/dd format as that is what is used on jw.org website.
+     * @return
+     * @throws IOException
+     */
+    public DailyTextEntity crawlForDailyText(String date) throws IOException, ParseException {
+        //retrieve html
+        Document doc = Jsoup.connect(JW_DAILY_TEXT_BASE_URL + "/" + date).get();
+
+        //Find paragraph(p) tag with themeScrp class in the html page,this contains the theme Scripture
+        Element dailyTextThemeParagraph = doc.select("p.themeScrp").first();
+        //Find the first div with the class 'bodyTxt' in the html page,this contains the body of the daily Text.
+        Element dailyTextBodyDiv = doc.select("div.bodyTxt").first();
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(DATE_PATTERN);
+        Date friendlyDate = simpleDateFormat.parse(date);
+        String friendlyDateString = simpleDateFormat.format(friendlyDate);
+
+        return new DailyTextEntity(friendlyDateString,dailyTextThemeParagraph.text(),dailyTextBodyDiv.text());
+
+    }
 }
